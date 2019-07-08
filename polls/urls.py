5521a3abe4d34f4370b6147b5e22871dd21b0b95
daseul.kim <da91love@gmail.com>
2019-07-09 00:33:10 +0900

    initial

---
+++ polls/urls.py
@@ -0,0 +1,19 @@
+"""django_tutorial URL Configuration
+polls application
+"""
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/specifics/5/
+    # the 'name' value as called by the {% url %} template tag
+    # added the word 'specifics'
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
